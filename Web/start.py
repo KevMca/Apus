f4167f24d4a857265636a528a59235f56cde24fb
Michael McAndrew <michael@inscribe.ai>
2020-07-31 19:22:08 +0100

    Add Web folder

---
+++ Web/start.py
@@ -0,0 +1,44 @@
+# Import microWebSrv and IMU libraries
+import os
+from microWebSrv import MicroWebSrv
+#os.chdir("..")
+#import orient
+#os.chdir("Web")
+
+# Web sockets
+# ----------------------------------------------------------------------------
+'''
+# Web socket initialisation
+def _acceptWebSocketCallback(webSocket, httpClient) :
+	print("WS ACCEPT")
+	webSocket.RecvTextCallback   = _recvTextCallback
+	webSocket.RecvBinaryCallback = _recvBinaryCallback
+	webSocket.ClosedCallback 	 = _closedCallback
+
+# Response to received
+def _recvTextCallback(webSocket, msg) :
+	print("WS RECV TEXT : %s" % msg)
+	matrix = orient.getOrient()
+	m_str = "{},{},{};{},{},{};{},{},{}".format(matrix.vect_x.vect[0], matrix.vect_x.vect[1], matrix.vect_x.vect[2], #east
+		matrix.vect_y.vect[0], matrix.vect_y.vect[1], matrix.vect_y.vect[2], #north
+		matrix.vect_z.vect[0], matrix.vect_z.vect[1], matrix.vect_z.vect[2]) #down
+	webSocket.SendText("%s" % m_str)
+
+# Print received message
+def _recvBinaryCallback(webSocket, data) :
+	print("WS RECV DATA : %s" % data)
+
+# Web socket closure
+def _closedCallback(webSocket) :
+	print("WS CLOSED")
+'''
+# Start server
+# ----------------------------------------------------------------------------
+
+srv = MicroWebSrv(webPath='www')
+srv.MaxWebSocketRecvLen     = 256
+srv.WebSocketThreaded		= False
+#srv.AcceptWebSocketCallback = _acceptWebSocketCallback
+srv.Start(threaded=True)
+
+# ----------------------------------------------------------------------------
